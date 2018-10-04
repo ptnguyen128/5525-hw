cfcc14c705706ffd13d929e82c14895447c6069a
Thao Nguyen <ptnguyen128@gmail.com>
2018-10-03 19:32:04 -0500

    update all files

---
+++ Hw1/GaussianModel.py
@@ -0,0 +1,49 @@
+import pandas as pd, numpy as np
+
+class GaussianModel():
+    def __init__(self, data):
+        '''
+        Class initializer
+        -------------------------------
+        '''
+        self.data = data
+        self.label = label
+
+        def to_dict(self, df):
+            '''
+            Function to turn a dataframe into a dictionary
+            '''
+            # get the groups
+            grouped = df.groupby(df.loc[:,self.label])
+            self.classes = [k for k in grouped.groups.keys()]
+
+            # mean for each class:
+            data_class = {}
+            X = {}
+            for k in self.classes:
+                data_class[k] = grouped.get_group(k)
+                X[k] = data_class[k].drop(self.label,axis=1)
+
+            return X
+
+        def calculate_gaussian(self, data):
+            '''
+            Function to perform generative Gaussian modeling and
+            calculate class prior, mean, covariance
+            '''
+            # Project the original data into n dimensions
+            X = self.to_dict(data)
+
+            g_prior = {}
+            g_mean = {}
+            g_cov = {}
+
+            # for each class
+            for k in X.keys():
+                # prior
+                g_prior[k] = X[k].shape[0] / data.shape[0]
+                # mean
+                g_mean[k] = np.mean(X[k], axis = 0)
+                # covariance
+                g_cov[k] = np.cov(X[k],rowvar=False)
+            return g_prior, g_mean, g_cov
--- Hw1/LogisticRegression.py
+++ Hw1/LogisticRegression.py
@@ -1,11 +1,10 @@
 import sys
 
 import pandas as pd, numpy as np
-import matplotlib.pyplot as plt
 import random
 
-from sklearn.datasets import load_digits
-from sklearn.datasets import load_boston
+from train_test_split import train_test_split
+from data import load_data
 
 class LogisticRegression():
 	def __init__(self, X, y):
@@ -13,136 +12,71 @@ class LogisticRegression():
 		Class initializer
 		-------------------------------------
 		Input:
-		data: the dataset
-		label: name of the target variable
+		X: input data
+		y: label data
 		-------------------------------------
 		'''
 		self.X = X
 		self.N, self.D = X.shape
-		# add a column of ones to X
-		ones = np.ones(N).reshape(1,N)
-		self.phi = np.concatenate((ones,self.X), axis=1)		# shape (N, D+1)
 
 		self.y = y
 		self.classes = np.unique(y)
 		self.K = len(self.classes)
 		# turn labels into one-hot-coding
-		self.t_one_hot = np.zeros((N,len(self.classes)))
-		self.t_one_hot[np.arange(N), self.y] = 1				# shape (N, K)
+		self.t_one_hot = np.zeros((self.N,len(self.classes)))
+		self.t_one_hot[np.arange(self.N), self.y] = 1				# shape (N, K)
 
 		self.max_iter = 50
 		self.threshold = 1e-10
 
 		# initialize weights
-		self.W = 0.001 * np.random.random((D+1, len(classes)))	# shape (D+1, K)
-
-
-	# Sigmoid function
-	def sigmoid(self, a):
-		return 1/(1 + np.exp(-a))
+		self.W = 0.001 * np.random.random((self.D, len(self.classes)))	# shape (D, K)
 
 	# Softmax function
-	def softmax(self, a_k):
+	def softmax(self, a):
 		e = np.exp(a - np.max(a, axis=1).reshape((-1,1)))
 		e_total = np.sum(e, axis=1).reshape((-1,1))
 		return e / e_total
 
-	def IRLS(self, data):
+	def IRLS(self):
+		'''
+		Function to update the weights iteratively
+		'''
 
 		for i in range(self.max_iter):
 			# activations
-			a = np.dot(self.phi, self.W)								# shape (N, K)
+			a = np.dot(self.X, self.W)						# shape (N, K)
 			# posterior
-			p = softmax(a)									# shape (N, K)
+			p = self.softmax(a)								# shape (N, K)
+			# gradient
+			grad = np.dot(self.X.T, (p - self.t_one_hot))		# shape (D, K)
 
-			# cross-entropy (we want to minimize this)
-			E = - np.sum(self.t_one_hot * np.log(p + 1e-6))	
-
-		return E
-
-	def binary_logreg(self, data):
-		'''
-		Function to update weights
-		'''
-		# input data and label
-		t = np.array(data[label])	# shape (N,)
-		X = data.drop(self.label, axis=1)	# shape (N, D)
-		# get dimensions of X
-		N = X.shape[0]		# number of observations (rows)
-		D = X.shape[1]		# number of features (columns)
-		# add a column of ones to X
-		ones = np.array([[1]*N]).T 					# shape (1, N)
-		phi = np.concatenate((ones,X), axis=1)		# shape (N, D+1)
-		# initialize the weights
-		w = np.zeros(D+1)		# shape (D+1,)
-		#w = np.random.randn(D+1)	# shape (D+1,)
-
-		for i in range(self.max_iter):
-			# get activations
-			a = np.dot(phi,w)			# shape (N,)
-			# posterior
-			p = self.sigmoid(a)			# shape (N,)
-
-			# cross-entropy (we want to minimize this)
-			E = - np.sum(t*safe_ln(p) + (1-t)*safe_ln(1-p))
+			# cross-entropy (to be minimized)
+			E = - np.sum(self.t_one_hot * np.log(p + 1e-6))
 
 			# weighing matrix
-			R = np.diag(p * (1 - p))	# shape (N, N)
+			R = np.diag(p[:,0]*p[:,1])
 
-			# update new w
-			z = np.dot(phi, w) - np.dot((p-t), np.linalg.pinv(R)) 		# shape (N,)
-			w = np.linalg.pinv(np.dot(np.dot(phi.T,R),phi)).dot(phi.T).dot(R).dot(z)	# shape (D+1,)
+			# update new weights
+			z = a - np.dot(np.linalg.pinv(R),(p-self.t_one_hot)) 	# shape (N,K)
+			H = np.dot(self.X.T, R).dot(self.X)							# shape (D,D)
+			self.W = np.linalg.pinv(H).dot(self.X.T).dot(R).dot(z)		# shape (D,K)
 
-			new_p = self.sigmoid(np.dot(phi,w))
-			new_E = -np.sum(t*safe_ln(new_p) + (1-t)*safe_ln(1-new_p))
+			new_p = self.softmax(np.dot(self.X, self.W))
+			new_E = - np.sum(self.t_one_hot * np.log(new_p + 1e-6))
 
-			print(new_E - E)
-
-			# If converge
-			if new_E - E < self.threshold:
-				print("Iteration: ", i)
-				print("Updated weight: ", w)
+			# stop if converge
+			if E - new_E < self.threshold:
 				break
 
-
-def to_label(data, target, percentile):
-	'''
-	Input: data, name of target column, percentile to partition data
-	Output: data, but with the target column values
-	changed from continuous to categorial (classes)
-	'''
-	frac = percentile / 100.0
-	part_val = data[target].quantile(frac)
-	data[target] = [1 if d > part_val else 0 for d in data[target]]
-	return data
-
-def train_test_split(data, label, test_ratio=0.2):
-	'''
-	Fuction to split the dataset into train-test sets
-	based on the specified size of the test set
-	'''
-	test_idx = []
-	indices = [i for i in range(data.shape[0])]
-
-	test_size = test_ratio * len(data)
-	while len(test_idx) < test_size:
-		test_idx.append(random.randrange(len(indices)))
-
-	train_idx = [i for i in indices if i not in test_idx]
-
-	test = data.iloc[test_idx]
-	train = data.iloc[train_idx]
-
-	y_train = train[label]
-	X_train = train.drop(label,axis=1)
-
-	y_test = test[label]
-	X_test = test.drop(label,axis=1)
-
-	return X_train, y_train, X_test, y_test
+	def calculate_error(self, X_test, y_test):
+		y_prob = self.softmax(np.dot(X_test, self.W))
+		y_pred = list(np.argmax(y_prob, axis=1))
+		error = np.sum(y_pred != y_test) / float(len(y_test))
+		return error
 
 
-def logisticRegression(filename):
+def logisticRegression(filename, num_splits, train_percent):
 	'''
 	Input:
 		filename: boston / digits
@@ -152,30 +86,36 @@ def logisticRegression(filename):
 	Output:
 		test set error rates for each training set percent
 	'''
-	if filename == 'digits':
-		# Load in the data
-		digits = load_digits()
-		data = pd.DataFrame(digits.data)
-		label = 'class'
-		data[label] = digits.target
+	data, label = load_data(filename)
 
-	elif filename == 'boston':
-		# Load in the data
-		boston = load_boston()
-		data = pd.DataFrame(boston.data, columns=boston.feature_names)
-		label = 'HomeVal50'
-		data[label] = boston.target
-		# Transform the target variable to binary
-		to_label(data, label, 50)
+	error_matrix = np.zeros((num_splits, len(train_percent)))
 
-	else:
-		print("Please enter a valid file name.")
+	for i in range(num_splits):
+		# Split the dataset into 80-20 train-test sets
+		X_train, y_train, X_test, y_test = train_test_split(data, label=label)
 
-	# Split the dataset into 80-20 train-test sets
-	X_train, y_train, X_test, y_test = train_test_split(data, label=label)
-	print(X_train.head(5))
+		for j,p in enumerate(train_percent):
+			# subset the training set
+			train_max_idx = int(np.floor(p/100.00 * X_train.shape[0]))
+			X_train_p = X_train.loc[:train_max_idx]
+			y_train_p = y_train.loc[:train_max_idx]
+
+			# fit Logistic Regression model
+			log_reg = LogisticRegression(X_train_p, y_train_p)
+			log_reg.IRLS()
+
+			# calculate test error
+			error_matrix[i,j] = log_reg.calculate_error(X_test, y_test)
+
+	mean_error = np.mean(error_matrix, axis=0)
+	std_error = np.std(error_matrix, axis=0, ddof=1)
+
+	return mean_error, std_error
 
 if __name__ == '__main__':
 	filename = sys.argv[1]
+	num_splits = int(sys.argv[2])
 
-	logisticRegression(filename)
+	mean_error, std_error = logisticRegression(filename, num_splits, [10, 25, 50, 75, 100])
+
+	print("Mean test errors are %s , with standard errors % s" % (mean_error, std_error))
